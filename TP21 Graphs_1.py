89f70ce95996dc4d461fbb5e731d7c4babdeefb6
VT <55559682+Victor-T2001@users.noreply.github.com>
2021-02-25 01:56:22 +0200

    Add files via upload

---
+++ TP21 Graphs_1.py
@@ -0,0 +1,35 @@
+import matplotlib.pyplot as plt
+from TP21_Downloading_data import *
+
+graph_names = ['^GSPC', '000001.SS', '^N100', '^HSI', '^KS11', '^N225']
+
+def draw_graph(data, name):
+
+	font = {'fontname' : 'Times New Roman', 
+			'size' : 20}
+
+	plt.figure(figsize=(20,10))
+	plt.plot(data["Date"], data["Close"], color='mediumblue', linewidth=1.1)
+
+	plt.tight_layout()
+	plt.gcf().subplots_adjust(top=0.9, bottom=0.13, left=0.08)
+
+	plt.ylabel('\nЗначення індексу', **font)
+	plt.xlabel('Дата', **font)
+	plt.title('Динаміка індексу '+ name, {'fontname' : 'Times New Roman', 'size' : 30})
+
+	ax = plt.axes()
+	ax.spines["right"].set_visible(False)
+	ax.spines["top"].set_visible(False)
+
+	for tick in ax.get_xticklabels():
+		tick.set_fontname("Times New Roman")
+	for tick in ax.get_yticklabels():
+		tick.set_fontname("Times New Roman")
+	ax.tick_params(axis='both', which='major', labelsize=20)
+
+	plt.savefig(name + ".jpg")
+	plt.close()
+
+for i in range(len(index_list)):
+	draw_graph(index_list[i], graph_names[i])
---
+++ TP21_Downloading_data.py
@@ -0,0 +1,23 @@
+import yfinance as yf
+from datetime import date, datetime
+import pandas as pd 
+
+start_date = date(2000, 2, 1)
+end_date = date(2021, 2, 24)
+
+names_list = ['^GSPC', '000001.SS', '^N100', '^HSI', '^KS11', '^N225']
+index_list = []
+
+for i in range(len(names_list)):
+	tickerSymbol = names_list[i]
+	tickerData = yf.Ticker(tickerSymbol)
+
+	df = tickerData.history(period='1d', start=start_date, end=end_date).dropna()
+	df.drop(['Stock Splits', 'Dividends'], axis='columns', inplace=True)
+	df = pd.DataFrame(data = df)
+	df.index = pd.to_datetime(df.index)
+	df["Date"] = df.index
+
+	df["Percent change"] = df["Close"].pct_change(periods=1)
+
+	index_list.append(df)
